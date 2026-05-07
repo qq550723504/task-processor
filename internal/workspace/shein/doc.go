edac3dbc9c7c02eeb890e3e5b37f21b1979794db
xuwei <550723504@qq.com>
2026-05-08 01:08:09 +0800

    Move SHEIN revision validation into workspace

---
+++ internal/workspace/shein/doc.go
@@ -0,0 +1,5 @@
+// Package shein owns SHEIN workspace editing, inspection, repair, revision,
+// and submit-readiness presentation helpers. ListingKit should depend on this
+// package through thin bridge/facade methods instead of keeping SHEIN workspace
+// rules in its root package.
+package shein
