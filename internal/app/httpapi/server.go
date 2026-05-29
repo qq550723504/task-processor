50271723fe4c8e13b9be38a33ff84cb24237a29b
xuwei <550723504@qq.com>
2026-05-29 17:16:06 +0800

    refactor: move legacy http server helpers to tests

--- internal/app/httpapi/server.go
+++ internal/app/httpapi/server.go
@@ -10,21 +10,6 @@ import (
 	listingkithttpapi "task-processor/internal/listingkit/httpapi"
 )
 
-func buildHTTPServer(port int, productHandler productRouteHandler, imageHandler imageRouteHandler, amazonListingHandler amazonListingRouteHandler, listingKitHandler listingKitRouteHandler, taskRPCHandler taskRPCRouteHandler, sdsCatalogHandlers ...sdsCatalogRouteHandler) *http.Server {
-	server, _ := buildHTTPServerBundleWithStudio(port, productHandler, imageHandler, amazonListingHandler, listingKitHandler, nil, nil, nil, nil, taskRPCHandler, sdsCatalogHandlers...)
-	return server
-}
-
-func buildHTTPServerWithStudio(port int, productHandler productRouteHandler, imageHandler imageRouteHandler, amazonListingHandler amazonListingRouteHandler, listingKitHandler listingKitRouteHandler, studioSessionHandler studioSessionRouteHandler, taskRPCHandler taskRPCRouteHandler, sdsCatalogHandlers ...sdsCatalogRouteHandler) *http.Server {
-	server, _ := buildHTTPServerBundleWithStudio(port, productHandler, imageHandler, amazonListingHandler, listingKitHandler, nil, studioSessionHandler, nil, nil, taskRPCHandler, sdsCatalogHandlers...)
-	return server
-}
-
-func buildHTTPServerBundleWithStudio(port int, productHandler productRouteHandler, imageHandler imageRouteHandler, amazonListingHandler amazonListingRouteHandler, listingKitHandler listingKitRouteHandler, promptTemplateHandler promptTemplateRouteHandler, studioSessionHandler studioSessionRouteHandler, sheinLoginHandler sheinLoginRouteHandler, sdsLoginHandler sdsLoginRouteHandler, taskRPCHandler taskRPCRouteHandler, sdsCatalogHandlers ...sdsCatalogRouteHandler) (*http.Server, []routeDescriptor) {
-	routes := buildRouteDescriptorsWithShein(productHandler, imageHandler, amazonListingHandler, listingKitHandler, promptTemplateHandler, studioSessionHandler, sheinLoginHandler, sdsLoginHandler, taskRPCHandler, sdsCatalogHandlers...)
-	return buildHTTPServerFromRoutes(port, routes), routes
-}
-
 func buildHTTPServerFromRoutes(port int, routes []routeDescriptor) *http.Server {
 	router := gin.New()
 	router.Use(gin.Recovery())
@@ -33,48 +18,6 @@ func buildHTTPServerFromRoutes(port int, routes []routeDescriptor) *http.Server 
 		Addr:              fmt.Sprintf(":%d", port),
 		Handler:           router,
 		ReadHeaderTimeout: 5 * time.Second,
-	}
-}
-
-func RegisterRoutes(r *gin.Engine, productHandler productRouteHandler, imageHandler imageRouteHandler, amazonListingHandler amazonListingRouteHandler, listingKitHandler listingKitRouteHandler, taskRPCHandler taskRPCRouteHandler, sdsCatalogHandlers ...sdsCatalogRouteHandler) {
-	mountRoutes(r, buildRouteDescriptorsWithShein(productHandler, imageHandler, amazonListingHandler, listingKitHandler, nil, nil, nil, nil, taskRPCHandler, sdsCatalogHandlers...))
-}
-
-func RegisterRoutesWithPrompt(r *gin.Engine, productHandler productRouteHandler, imageHandler imageRouteHandler, amazonListingHandler amazonListingRouteHandler, listingKitHandler listingKitRouteHandler, promptTemplateHandler promptTemplateRouteHandler, taskRPCHandler taskRPCRouteHandler, sdsCatalogHandlers ...sdsCatalogRouteHandler) {
-	mountRoutes(r, buildRouteDescriptorsWithShein(productHandler, imageHandler, amazonListingHandler, listingKitHandler, promptTemplateHandler, nil, nil, nil, taskRPCHandler, sdsCatalogHandlers...))
-}
-
-func buildRouteDescriptors(productHandler productRouteHandler, imageHandler imageRouteHandler, amazonListingHandler amazonListingRouteHandler, listingKitHandler listingKitRouteHandler, studioSessionHandler studioSessionRouteHandler, taskRPCHandler taskRPCRouteHandler, sdsCatalogHandlers ...sdsCatalogRouteHandler) []routeDescriptor {
-	return buildRouteDescriptorsWithShein(productHandler, imageHandler, amazonListingHandler, listingKitHandler, nil, studioSessionHandler, nil, nil, taskRPCHandler, sdsCatalogHandlers...)
-}
-
-func buildRouteDescriptorsWithShein(productHandler productRouteHandler, imageHandler imageRouteHandler, amazonListingHandler amazonListingRouteHandler, listingKitHandler listingKitRouteHandler, promptTemplateHandler promptTemplateRouteHandler, studioSessionHandler studioSessionRouteHandler, sheinLoginHandler sheinLoginRouteHandler, sdsLoginHandler sdsLoginRouteHandler, taskRPCHandler taskRPCRouteHandler, sdsCatalogHandlers ...sdsCatalogRouteHandler) []routeDescriptor {
-	routes, err := buildRegisteredRoutes(nil, httpModuleHandlers{
-		product:        productHandler,
-		image:          imageHandler,
-		amazonListing:  amazonListingHandler,
-		listingKit:     listingKitHandler,
-		promptTemplate: promptTemplateHandler,
-		studioSession:  studioSessionHandler,
-		sheinLogin:     sheinLoginHandler,
-		sdsLogin:       sdsLoginHandler,
-		taskRPC:        taskRPCHandler,
-		sdsCatalog:     singleSDSCatalogHandler(sdsCatalogHandlers...),
-	})
-	if err != nil {
-		panic(err)
-	}
-	return routes
-}
-
-func singleSDSCatalogHandler(sdsCatalogHandlers ...sdsCatalogRouteHandler) sdsCatalogRouteHandler {
-	switch len(sdsCatalogHandlers) {
-	case 0:
-		return nil
-	case 1:
-		return sdsCatalogHandlers[0]
-	default:
-		panic(fmt.Sprintf("expected at most 1 SDS catalog handler, got %d", len(sdsCatalogHandlers)))
 	}
 }
 
