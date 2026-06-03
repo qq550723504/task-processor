6a07ccd3f1a2308a15010401642cd112d371e9c4
xuwei <550723504@qq.com>
2026-06-03 12:37:02 +0800

    test: lock listingkit review navigation queue clone boundaries

---
+++ internal/listingkit/phase24_review_navigation_queue_clone_boundary_test.go
@@ -0,0 +1,70 @@
+package listingkit
+
+import "testing"
+
+func TestGenerationReviewNavigationQueueCloneBoundary(t *testing.T) {
+	t.Parallel()
+
+	t.Run("review_navigation_builder_routes_shared_queue_clone_through_common_home", func(t *testing.T) {
+		t.Parallel()
+
+		source := readNamedFunctionSource(t, "generation_review_navigation_target.go", "buildGenerationReviewActionNavigationTarget")
+		callNames := readNamedFunctionCallNames(t, "generation_review_navigation_target.go", "buildGenerationReviewActionNavigationTarget")
+
+		assertSourceContainsAll(t, source, []string{
+			"cloneAssetGenerationActionTargetForNavigation(target)",
+			"cloneGenerationQueueQuery(target.QueueQuery)",
+			"applyIdentityToNavigationTarget(navigation)",
+		})
+		assertSourceExcludesAll(t, source, []string{
+			"cloneAssetGenerationActionTarget(target)",
+			"cloned := *target.QueueQuery",
+			"navigation.QueueQuery = &cloned",
+			"cloneAssetGenerationFilters(",
+			"cloneRetryGenerationTasksRequest(",
+			"cloneAssetGenerationActionImpact(",
+		})
+		assertFunctionCallsContainAll(t, callNames, []string{
+			"cloneAssetGenerationActionTargetForNavigation",
+			"cloneGenerationQueueQuery",
+			"applyIdentityToNavigationTarget",
+		})
+		assertFunctionCallsAppearInOrder(t, callNames, []string{
+			"cloneAssetGenerationActionTargetForNavigation",
+			"cloneGenerationQueueQuery",
+			"applyIdentityToNavigationTarget",
+		})
+		assertFunctionCallsExcludeAll(t, callNames, []string{
+			"cloneAssetGenerationActionTarget",
+			"cloneAssetGenerationFilters",
+			"cloneRetryGenerationTasksRequest",
+			"cloneAssetGenerationActionImpact",
+		})
+	})
+
+	t.Run("shared_queue_clone_home_stays_outside_review_navigation_local_home", func(t *testing.T) {
+		t.Parallel()
+
+		navigationSource := readTaskGenerationSourceFile(t, "generation_review_navigation_target.go")
+		serviceSource := readTaskGenerationSourceFile(t, "service_generation_actions.go")
+
+		assertSourceContainsAll(t, navigationSource, []string{
+			"func buildGenerationReviewActionNavigationTarget(",
+			"func cloneAssetGenerationActionTargetForNavigation(",
+			"cloneGenerationQueueQuery(target.QueueQuery)",
+		})
+		assertSourceExcludesAll(t, navigationSource, []string{
+			"func cloneGenerationQueueQuery(",
+			"func cloneRetryGenerationTasksRequest(",
+		})
+
+		assertSourceContainsAll(t, serviceSource, []string{
+			"func cloneGenerationQueueQuery(",
+			"func cloneRetryGenerationTasksRequest(",
+		})
+		assertSourceExcludesAll(t, serviceSource, []string{
+			"func buildGenerationReviewActionNavigationTarget(",
+			"func cloneAssetGenerationActionTargetForNavigation(",
+		})
+	})
+}
