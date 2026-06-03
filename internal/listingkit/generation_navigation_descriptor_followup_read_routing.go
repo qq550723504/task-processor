08862193b04872cf0c5baaae9cf0638b6fc9a181
xuwei <550723504@qq.com>
2026-06-03 19:53:26 +0800

    refactor: clarify listingkit descriptor follow-up read routing pairing ownership

--- internal/listingkit/generation_navigation_descriptor_followup_read_routing.go
+++ internal/listingkit/generation_navigation_descriptor_followup_read_routing.go
@@ -4,11 +4,5 @@ func applyGenerationNavigationDescriptorFollowUpReadCloneRouting(descriptor *Gen
 	if descriptor == nil || cloned == nil {
 		return
 	}
-	if len(descriptor.FollowUpReads) == 0 {
-		return
-	}
-	cloned.FollowUpReads = make([]GenerationNavigationFollowUpRead, 0, len(descriptor.FollowUpReads))
-	for _, item := range descriptor.FollowUpReads {
-		cloned.FollowUpReads = append(cloned.FollowUpReads, cloneGenerationNavigationFollowUpRead(item))
-	}
+	applyGenerationNavigationDescriptorFollowUpReadRoutingPairing(descriptor, cloned)
 }
---
+++ internal/listingkit/generation_navigation_descriptor_followup_read_routing_pairing.go
@@ -0,0 +1,14 @@
+package listingkit
+
+func applyGenerationNavigationDescriptorFollowUpReadRoutingPairing(descriptor *GenerationNavigationDescriptor, cloned *GenerationNavigationDescriptor) {
+	if descriptor == nil || cloned == nil {
+		return
+	}
+	if len(descriptor.FollowUpReads) == 0 {
+		return
+	}
+	cloned.FollowUpReads = make([]GenerationNavigationFollowUpRead, 0, len(descriptor.FollowUpReads))
+	for _, item := range descriptor.FollowUpReads {
+		cloned.FollowUpReads = append(cloned.FollowUpReads, cloneGenerationNavigationFollowUpRead(item))
+	}
+}
