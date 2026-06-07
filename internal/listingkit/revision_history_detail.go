e41fededfd30808f978be92eb61889ee0104c0e6
xuwei <550723504@qq.com>
2026-06-07 18:37:16 +0800

    refactor(listingkit): move restore preview presentation into detail home

--- internal/listingkit/revision_history_detail.go
+++ internal/listingkit/revision_history_detail.go
@@ -85,6 +85,40 @@ func buildRevisionHistoryRestorePayload(record *ListingKitRevisionRecord) *Apply
 	}
 }
 
+func buildRevisionRestorePreviewPresentation(
+	record *ListingKitRevisionRecord,
+	context *RevisionHistoryRestoreContext,
+	safety *RevisionHistoryRestoreSafety,
+	comparePreview *RevisionHistoryComparePreview,
+) *RevisionInteractionPresentation {
+	data := buildRevisionHistoryRestorePresentationData(record, context, safety, comparePreview)
+	if data == nil {
+		return nil
+	}
+
+	summaryCard := &RevisionSuccessSummaryCard{
+		Status:        data.Status,
+		Title:         data.Headline,
+		Subtitle:      data.Subheadline,
+		PrimaryAction: data.PrimaryAction,
+		Highlights:    append([]string(nil), data.Highlights...),
+	}
+
+	return sheinworkspace.BuildSuccessPresentation(
+		revisionPresentationSceneRestorePreview,
+		append([]string(nil), data.NextActions...),
+		&RevisionResultMessages{
+			Title:            data.Title,
+			Description:      data.Description,
+			SuccessLabel:     data.ConfirmLabel,
+			WarningTitle:     data.WarningTitle,
+			WarningSummaries: append([]string(nil), data.WarningSummaries...),
+		},
+		convertHistoryRestoreRecommendedView(data.RecommendedView),
+		summaryCard,
+	)
+}
+
 func normalizeRevisionHistoryDetailQuery(query *RevisionHistoryDetailQuery) RevisionHistoryDetailQuery {
 	if query == nil {
 		return RevisionHistoryDetailQuery{}
