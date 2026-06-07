e3bca3bba64abd1fb5381c8ab10764fcc6fd7e77
xuwei <550723504@qq.com>
2026-06-07 18:30:40 +0800

    refactor(listingkit): move revision restore payload into detail home

--- internal/listingkit/revision_history_detail.go
+++ internal/listingkit/revision_history_detail.go
@@ -69,6 +69,22 @@ func buildRevisionHistoryDetail(result *ListingKitResult, revisionID string, que
 	return nil, ErrRevisionHistoryRecordNotFound
 }
 
+func buildRevisionHistoryRestorePayload(record *ListingKitRevisionRecord) *ApplyRevisionRequest {
+	if record == nil {
+		return nil
+	}
+	revisionID := record.RevisionID
+	if revisionID == "" {
+		return nil
+	}
+	return &ApplyRevisionRequest{
+		Platform:              firstNonEmpty(record.Platform, "shein"),
+		Actor:                 "desktop-client",
+		Reason:                buildRevisionHistoryRestoreReason(record),
+		RestoreFromRevisionID: revisionID,
+	}
+}
+
 func normalizeRevisionHistoryDetailQuery(query *RevisionHistoryDetailQuery) RevisionHistoryDetailQuery {
 	if query == nil {
 		return RevisionHistoryDetailQuery{}
