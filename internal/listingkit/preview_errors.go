58e76b8186f26e5b3e38058bbe3b037a190ab948
xuwei <550723504@qq.com>
2026-06-12 21:18:36 +0800

    refactor(listingkit-preview): share preview error definitions

--- internal/listingkit/preview_errors.go
+++ internal/listingkit/preview_errors.go
@@ -1,8 +1,12 @@
 package listingkit
 
-import "errors"
+import (
+	"errors"
 
-var ErrUnsupportedPreviewPlatform = errors.New("unsupported preview platform")
-var ErrPreviewPlatformUnavailable = errors.New("preview platform unavailable")
-var ErrTaskResultUnavailable = errors.New("task result unavailable")
+	previewdomain "task-processor/internal/listing/preview"
+)
+
+var ErrUnsupportedPreviewPlatform = previewdomain.ErrUnsupportedPlatform
+var ErrPreviewPlatformUnavailable = previewdomain.ErrPlatformUnavailable
+var ErrTaskResultUnavailable = previewdomain.ErrResultUnavailable
 var ErrInvalidRevisionRequest = errors.New("invalid revision request")
