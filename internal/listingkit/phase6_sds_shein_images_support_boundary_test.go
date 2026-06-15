826ccf93ed6d77776a63456ee5ca1f9fe955135f
xuwei <550723504@qq.com>
2026-06-15 13:23:54 +0800

    refactor: split sds shein image support

---
+++ internal/listingkit/phase6_sds_shein_images_support_boundary_test.go
@@ -0,0 +1,39 @@
+package listingkit
+
+import "testing"
+
+func TestSDSSheinImagesSupportBoundary(t *testing.T) {
+	t.Parallel()
+
+	rootSource := readTaskGenerationSourceFile(t, "sds_shein_images.go")
+	assertSourceContainsAll(t, rootSource, []string{
+		"func applySelectedSDSImagesToShein(pkg *sheinpub.Package, req *GenerateRequest, sourceImages []string) bool {",
+		"func applySDSOfficialImagesToShein(pkg *sheinpub.Package, _ *GenerateRequest, summary *SDSSyncSummary, options *SDSSyncOptions) bool {",
+		"func applySDSTemplateImagesToShein(pkg *sheinpub.Package, summary *SDSSyncSummary, sourceImages []string, options ...*SDSSyncOptions) {",
+		"func applySDSTemplateImagesToSheinWithResult(pkg *sheinpub.Package, summary *SDSSyncSummary, sourceImages []string, options *SDSSyncOptions) bool {",
+		"func applySDSVariantTemplateImagesToShein(pkg *sheinpub.Package, summary *SDSSyncSummary, sourceImages []string, options *SDSSyncOptions) bool {",
+		"func hasSDSVariantOptionMockups(options *SDSSyncOptions) bool {",
+	})
+	assertSourceExcludesAll(t, rootSource, []string{
+		"func resolveSDSImagesForSKC(pkg *sheinpub.Package, index int, bySKU map[string]*common.ImageSet, byColor map[string]*common.ImageSet) *common.ImageSet {",
+		"func sdsSKUCandidatesFromRequestSKC(skc *sheinpub.SKCRequestDraft) []string {",
+		"func imageSetFromSDSMockups(mockups []string, sourceImages []string) *common.ImageSet {",
+		"func normalizeSelectedSDSImages(input []SheinStudioSelectedSDSImage) []SheinStudioSelectedSDSImage {",
+		"func normalizeSDSColorKey(value string) string {",
+	})
+
+	supportSource := readTaskGenerationSourceFile(t, "sds_shein_images_support.go")
+	assertSourceContainsAll(t, supportSource, []string{
+		"func resolveSDSImagesForSKC(pkg *sheinpub.Package, index int, bySKU map[string]*common.ImageSet, byColor map[string]*common.ImageSet) *common.ImageSet {",
+		"func sdsSKUCandidatesFromRequestSKC(skc *sheinpub.SKCRequestDraft) []string {",
+		"func imageSetFromSDSMockups(mockups []string, sourceImages []string) *common.ImageSet {",
+		"func normalizeSelectedSDSImages(input []SheinStudioSelectedSDSImage) []SheinStudioSelectedSDSImage {",
+		"func normalizeSDSColorKey(value string) string {",
+	})
+	assertSourceExcludesAll(t, supportSource, []string{
+		"func applySelectedSDSImagesToShein(pkg *sheinpub.Package, req *GenerateRequest, sourceImages []string) bool {",
+		"func applySDSOfficialImagesToShein(pkg *sheinpub.Package, _ *GenerateRequest, summary *SDSSyncSummary, options *SDSSyncOptions) bool {",
+		"func applySDSTemplateImagesToSheinWithResult(pkg *sheinpub.Package, summary *SDSSyncSummary, sourceImages []string, options *SDSSyncOptions) bool {",
+		"func applySDSVariantTemplateImagesToShein(pkg *sheinpub.Package, summary *SDSSyncSummary, sourceImages []string, options *SDSSyncOptions) bool {",
+	})
+}
