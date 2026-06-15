da8ea47f59017568db137e4cb40f53eb645b8aec
xuwei <550723504@qq.com>
2026-06-15 14:13:06 +0800

    refactor: split shein repair clone support

---
+++ internal/listingkit/phase6_shein_repair_clone_support_boundary_test.go
@@ -0,0 +1,82 @@
+package listingkit
+
+import (
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestSheinRepairCloneSupportBoundary(t *testing.T) {
+	t.Parallel()
+
+	rootSrc, err := os.ReadFile("shein_repair_support.go")
+	if err != nil {
+		t.Fatalf("ReadFile(shein_repair_support.go) error = %v", err)
+	}
+	rootContent := string(rootSrc)
+
+	for _, needle := range []string{
+		"type SheinRepairValidationPreview = listingworkspace.RepairValidationPreview[RevisionFieldError]",
+		"type SheinRepairPatchPayload struct {",
+		"type sheinRepairRevisionBundle struct {",
+		"type sheinRepairArtifacts struct {",
+		"func buildSheinRepairRevisionBundle(action string, payload *SheinRepairPatchPayload) sheinRepairRevisionBundle {",
+		"func buildSheinRepairRevisionSkeleton(action string, payload *SheinRepairPatchPayload) *SheinEditorRevisionSkeleton {",
+		"func buildSheinRepairApplyRequest(action string, payload *SheinRepairPatchPayload) *ApplyRevisionRequest {",
+		"func buildSheinRepairRevisionInput(payload *SheinRepairPatchPayload) *SheinRevisionInput {",
+		"func buildSheinRepairReason(action string) string {",
+		"func buildSheinRepairArtifacts(pkg *SheinPackage, action string, editorSection string, patch *SheinRepairPatchPayload) sheinRepairArtifacts {",
+	} {
+		if !strings.Contains(rootContent, needle) {
+			t.Fatalf("shein_repair_support.go should contain %q", needle)
+		}
+	}
+
+	for _, needle := range []string{
+		"func clonePlatformImageSetForEditor(set *PlatformImageSet) *PlatformImageSet {",
+		"func cloneSheinRepairPatchPayload(payload *SheinRepairPatchPayload) *SheinRepairPatchPayload {",
+		"func cloneSheinCategoryResolutionPatch(patch *SheinCategoryResolutionPatch) *SheinCategoryResolutionPatch {",
+		"func cloneSheinAttributeResolutionPatch(patch *SheinAttributeResolutionPatch) *SheinAttributeResolutionPatch {",
+		"func cloneSheinSaleAttributeResolutionPatch(patch *SheinSaleAttributeResolutionPatch) *SheinSaleAttributeResolutionPatch {",
+		"func cloneSheinSKCRevisionPatches(items []SheinSKCRevisionPatch) []SheinSKCRevisionPatch {",
+		"func cloneSheinSKURevisionPatches(items []SheinSKURevisionPatch) []SheinSKURevisionPatch {",
+		"func cloneSheinResolvedSaleAttributePointer(attr *SheinResolvedSaleAttribute) *SheinResolvedSaleAttribute {",
+		"func cloneRepairStringPointer(value *string) *string {",
+		"func cloneRepairIntPointer(value *int) *int {",
+		"func cloneSheinRepairArtifacts(patch *SheinRepairPatchPayload, skeleton *SheinEditorRevisionSkeleton, request *ApplyRevisionRequest, validation *SheinRepairValidationPreview) sheinRepairArtifacts {",
+		"func buildSheinRepairValidationPreview(pkg *SheinPackage, editorSection string, revision *ApplyRevisionRequest, skeleton *SheinEditorRevisionSkeleton) *SheinRepairValidationPreview {",
+		"func cloneSheinRepairValidationPreview(src *SheinRepairValidationPreview) *SheinRepairValidationPreview {",
+		"func cloneRevisionDiffPreview(src *RevisionDiffPreview) *RevisionDiffPreview {",
+	} {
+		if strings.Contains(rootContent, needle) {
+			t.Fatalf("shein_repair_support.go should delegate clone support helper %q", needle)
+		}
+	}
+
+	supportSrc, err := os.ReadFile("shein_repair_clone_support.go")
+	if err != nil {
+		t.Fatalf("ReadFile(shein_repair_clone_support.go) error = %v", err)
+	}
+	supportContent := string(supportSrc)
+
+	for _, needle := range []string{
+		"func clonePlatformImageSetForEditor(set *PlatformImageSet) *PlatformImageSet {",
+		"func cloneSheinRepairPatchPayload(payload *SheinRepairPatchPayload) *SheinRepairPatchPayload {",
+		"func cloneSheinCategoryResolutionPatch(patch *SheinCategoryResolutionPatch) *SheinCategoryResolutionPatch {",
+		"func cloneSheinAttributeResolutionPatch(patch *SheinAttributeResolutionPatch) *SheinAttributeResolutionPatch {",
+		"func cloneSheinSaleAttributeResolutionPatch(patch *SheinSaleAttributeResolutionPatch) *SheinSaleAttributeResolutionPatch {",
+		"func cloneSheinSKCRevisionPatches(items []SheinSKCRevisionPatch) []SheinSKCRevisionPatch {",
+		"func cloneSheinSKURevisionPatches(items []SheinSKURevisionPatch) []SheinSKURevisionPatch {",
+		"func cloneSheinResolvedSaleAttributePointer(attr *SheinResolvedSaleAttribute) *SheinResolvedSaleAttribute {",
+		"func cloneRepairStringPointer(value *string) *string {",
+		"func cloneRepairIntPointer(value *int) *int {",
+		"func cloneSheinRepairArtifacts(patch *SheinRepairPatchPayload, skeleton *SheinEditorRevisionSkeleton, request *ApplyRevisionRequest, validation *SheinRepairValidationPreview) sheinRepairArtifacts {",
+		"func buildSheinRepairValidationPreview(pkg *SheinPackage, editorSection string, revision *ApplyRevisionRequest, skeleton *SheinEditorRevisionSkeleton) *SheinRepairValidationPreview {",
+		"func cloneSheinRepairValidationPreview(src *SheinRepairValidationPreview) *SheinRepairValidationPreview {",
+		"func cloneRevisionDiffPreview(src *RevisionDiffPreview) *RevisionDiffPreview {",
+	} {
+		if !strings.Contains(supportContent, needle) {
+			t.Fatalf("shein_repair_clone_support.go should contain %q", needle)
+		}
+	}
+}
