f9cd333dcd59ab358b4a4981531d43fef90d2783
xuwei <550723504@qq.com>
2026-06-25 00:27:28 +0800

    test: update listing kit route handler stub

--- internal/app/httpapi/shein_resolution_cache_route_test_stub_test.go
+++ internal/app/httpapi/shein_resolution_cache_route_test_stub_test.go
@@ -5,3 +5,27 @@ import "github.com/gin-gonic/gin"
 func (h *stubListingKitHandler) ClearSheinResolutionCache(c *gin.Context) {}
 
 func (h *stubListingKitHandler) RegenerateSheinDataImage(c *gin.Context) {}
+
+func (h *stubListingKitHandler) CreateSDSRetirementRun(c *gin.Context) {}
+
+func (h *stubListingKitHandler) GetSDSRetirementRun(c *gin.Context) {}
+
+func (h *stubListingKitHandler) UpdateSDSRetirementSelection(c *gin.Context) {}
+
+func (h *stubListingKitHandler) ConfirmSDSRetirementRun(c *gin.Context) {}
+
+func (h *stubListingKitHandler) RetrySDSRetirementRun(c *gin.Context) {}
+
+func (h *stubListingKitHandler) ListSheinSDSCostGroups(c *gin.Context) {}
+
+func (h *stubListingKitHandler) UpdateSheinSDSCostGroup(c *gin.Context) {}
+
+func (h *stubListingKitHandler) CreateStudioBatchRun(c *gin.Context) {}
+
+func (h *stubListingKitHandler) GetStudioBatchRun(c *gin.Context) {}
+
+func (h *stubListingKitHandler) ListStudioBatchRunItems(c *gin.Context) {}
+
+func (h *stubListingKitHandler) CancelStudioBatchRun(c *gin.Context) {}
+
+func (h *stubListingKitHandler) RecoverStudioBatchRun(c *gin.Context) {}
