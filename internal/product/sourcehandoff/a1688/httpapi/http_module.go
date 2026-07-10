2884842ffbab628a7d12c9bdab6c2337bddb776e
wei xu <xuweixia@live.com>
2026-07-10 11:25:02 +0800

    feat: add 1688 source handoff http module

---
+++ internal/product/sourcehandoff/a1688/httpapi/http_module.go
@@ -0,0 +1,40 @@
+package httpapi
+
+import (
+	"task-processor/internal/core/config"
+	"task-processor/internal/kernel/module"
+)
+
+type BuildResult struct {
+	Handler *Handler
+	Module  module.Module
+}
+
+type routeModule struct {
+	handler *Handler
+}
+
+func BuildModule(service TaskCommandService) *BuildResult {
+	handler := NewHandler(service)
+	return &BuildResult{
+		Handler: handler,
+		Module:  NewHTTPModule(handler),
+	}
+}
+
+func NewHTTPModule(handler *Handler) module.Module {
+	return routeModule{handler: handler}
+}
+
+func (m routeModule) Name() string {
+	return ModuleName
+}
+
+func (m routeModule) Enabled(*config.Config) bool {
+	return true
+}
+
+func (m routeModule) Register(reg *module.Registry) error {
+	reg.AddRoutes(AppendRouteDescriptors(nil, m.handler)...)
+	return nil
+}
